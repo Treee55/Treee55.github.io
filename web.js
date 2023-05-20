56e2eec0d08bb2f6a28d7eba067237288aa7793b
Treee55 <parkerspaan@yahoo.com>
2023-05-19 22:40:38 -0400

    contact is set up more or less

--- web.js
+++ web.js
@@ -5,3 +5,10 @@ menu.addEventListener('click',function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+function openContactWindow() {
+    // Open a new window with the desired text
+    var contactText = "This is the contact information.";
+    var contactWindow = window.open("", "_blank", "width=400,height=300");
+    contactWindow.document.write("<p>" + contactText + "</p>");
+}
